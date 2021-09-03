e969fb409351b635b2be68e112bb6280b147fe5c
Aqsa Aman <aqsaaman9@gmail.com>
2021-09-03 17:06:57 +0500

    print the elements of an array in reverse order

---
+++ ReverseArray.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class ReverseArray
+{
+	public static void main(String arg[])
+	{		
+		int arrRev [] = {1,2,3,4,5};
+		
+		for (int i=(arrRev.length-1); i>=0; i--){
+			System.out.println("Array Element: "+arrRev[i]);
+		}
+		
+	}
+	
+}
