b8c0c861335e34297a96591e4d0545377edfcfde
Aqsa Aman <aqsaaman9@gmail.com>
2021-09-03 16:51:13 +0500

    print elements of an array

---
+++ ArrayPrint.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class ArrayPrint
+{
+	public static void main(String arg[])
+	{
+		int arrPrint [] = {1,2,3,4,5};
+		
+		for (int i=0; i<arrPrint.length; i++){
+			System.out.println("Array Element: "+arrPrint[i]);
+		}
+	}
+	
+}
